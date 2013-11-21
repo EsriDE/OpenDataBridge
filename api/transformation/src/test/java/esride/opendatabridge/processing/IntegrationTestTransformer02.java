514623ea7d3905c67e98ce80d5d36c6632d54c2a
Markus Stecker <m.stecker@conterra.de>
2013-11-21 13:47:33 +0100

    New Delete Skript

--- api/transformation/src/test/java/esride/opendatabridge/processing/IntegrationTestTransformer02.java
+++ api/transformation/src/test/java/esride/opendatabridge/processing/IntegrationTestTransformer02.java
@@ -32,6 +32,35 @@ public class IntegrationTestTransformer02 extends AbstractJUnit4SpringContextTes
     @Autowired
     private IAgolService agolService;
 
+    //@Test
+    public void testRequestFiles(){
+        StartParameter param = null;
+        String[] paramArray = new String[5];
+        paramArray[0] = "-pid=Test01";
+        paramArray[1] = "-readerid=ckan";
+        paramArray[2] = "-searchstring=";
+        paramArray[3] = "-accesstype=PRIVATE";
+        paramArray[4] = "-ownertype=USER";
+        try {
+            param = new StartParameter(paramArray);
+        } catch (StartParameterException e) {
+            Assert.fail(e.getMessage());
+        }
+
+        IReader reader = null;
+        try {
+            reader = readerFactory.newReaderInstance(param.getReaderValue(), processInfo.getProperties(param.getPidValue()), param.getPidValue());
+        } catch (ReaderException e) {
+            Assert.fail(e.getMessage());
+        }
+        Transformer transform = new Transformer();
+        try {
+            transform.executeProcessDeleteDuplicate(agolService, param.getSearchStringValue(), param.getAccessTypeValue(), param.getOwnerTypeValue());
+        } catch (TransformerException e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+
     @Test
     public void testInsertKml(){
         StartParameter param = null;
@@ -39,7 +68,7 @@ public class IntegrationTestTransformer02 extends AbstractJUnit4SpringContextTes
         paramArray[0] = "-pid=Test01";
         paramArray[1] = "-readerid=ckan";
         paramArray[2] = "-searchstring=";
-        paramArray[3] = "-accesstype=PRIVATE";
+        paramArray[3] = "-accesstype=PUBLIC";
         paramArray[4] = "-ownertype=USER";
         try {
             param = new StartParameter(paramArray);
